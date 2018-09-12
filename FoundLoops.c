b5c234f00fc2a94db264f86eb17eee277b558c73
Timotej Kapus <tk1713@ic.ac.uk>
2018-09-12 11:48:09 +0100

    Add sed loops

--- FoundLoops.c
+++ FoundLoops.c
@@ -859,3 +859,30 @@ for (; p >= end; --p)
 	}
 }
 
+//sed-4.4/sed/compile.c:306:3
+  for (p=buf+1; p < bufend && max <= 255; ++p, max *= base)
+    {    
+      int d = -1;
+      switch (*p) 
+        {
+        case '0': d = 0x0; break;
+        case '1': d = 0x1; break;
+        case '2': d = 0x2; break;
+        case '3': d = 0x3; break;
+        case '4': d = 0x4; break;
+        case '5': d = 0x5; break;
+        case '6': d = 0x6; break;
+        case '7': d = 0x7; break;
+        case '8': d = 0x8; break;
+        case '9': d = 0x9; break;
+        case 'A': case 'a': d = 0xa; break;
+        case 'B': case 'b': d = 0xb; break;
+        case 'C': case 'c': d = 0xc; break;
+        case 'D': case 'd': d = 0xd; break;
+        case 'E': case 'e': d = 0xe; break;
+        case 'F': case 'f': d = 0xf; break;
+        }
+      if (d < 0 || base <= d)
+        break;
+      n = n * base + d; 
+    } 
