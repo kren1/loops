5e32ec5966d6f0478926683cb33944e61c430bde
Timotej Kapus <tk1713@ic.ac.uk>
2018-09-17 14:28:05 +0100

    Diffutils

---
+++ gdLoops/diffutils_ifdef_316.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#define N 10
+
+char *loopFunction(char *f) {
+  // diffutils-3.6/src/ifdef.c:316:3
+  char c;
+  while ((c = *f++) == '-' || c == '\'' || c == '0')
+    continue;
+  return f;
+}
+
+void driver() {
+  char str[N];
+  klee_make_symbolic(str, sizeof(str), "str");
+
+  char *p = loopFunction(str);
+#ifdef DRIVER
+#include "driver.c"
+#endif
+}
+
+#ifdef MAIN_FUNC
+#include "main.c"
+#endif
