306f1a42a6476c265ffc562ebae7f64d0595f540
Timotej Kapus <tk1713@ic.ac.uk>
2018-09-19 13:54:52 +0100

    Add more functions

--- gdLoops/synthesize/interpreter.c
+++ gdLoops/synthesize/interpreter.c
@@ -2,6 +2,8 @@
 #define EXAMPLE_MAX_SIZE 6
 
 #define STR_R_CHR 'r'
+#define STR_CHR 'c'
+#define STR_P_BRK 'b'
 #define IS_NULL 'z'
 #define SET_TO_START 's'
 #define END '\0'
@@ -15,13 +17,32 @@ char *interpreter(char* s, char* prog) {
     //If this flag is set to 0 the next instruction should be skipped
     int condition_flag = 1;
 
+    char str_buf[PROGRAM_MAX_SIZE] = {0};
+
 #define condition_check {if(condition_flag == 0) {condition_flag = 1; break;}}
     while((i < (PROGRAM_MAX_SIZE - 1))) {
         switch(prog[i]) {
+            case STR_CHR: 
+              condition_check
+              i++;
+              result = strchr(s, prog[i]);
+              break;
             case STR_R_CHR: 
               condition_check
               i++;
               result = strrchr(s, prog[i]);
+              break;
+            case STR_P_BRK: 
+              condition_check
+              i++;
+              int j = 0;
+              do {
+                str_buf[j] = prog[i];
+                i++, j++;
+              } while(str_buf[j] != '\0' && j < PROGRAM_MAX_SIZE);
+              result = strchr(s, str_buf);
+
+              memset(str_buf, 0, PROGRAM_MAX_SIZE);
               break;
             case IS_NULL:
               condition_check
