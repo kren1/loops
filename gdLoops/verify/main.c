15b96b58675a207e61668928173ed38d4b7e4def
Timotej Kapus <tk1713@ic.ac.uk>
2018-09-25 15:02:06 +0100

    Change verifcation infrastrcture

--- gdLoops/verify/main.c
+++ gdLoops/verify/main.c
@@ -7,13 +7,23 @@ int main(int argc, char** argv) {
     char *s = malloc(EXAMPLE_MAX_SIZE);
     char *o, *m;
 
+
+    char prog[PROGRAM_MAX_SIZE + 1];
+    prog[PROGRAM_MAX_SIZE] = '\0';
+    FILE *f = fopen(argv[1], "rb");
+    assert(f && "can't read file");
+    int n = fread(prog,1,PROGRAM_MAX_SIZE,f);
+    assert(n == PROGRAM_MAX_SIZE && "didn't read the whole program");
+    fclose(f);
+    printf("Running on %d  program %s\n",n,  prog);
+
     klee_make_symbolic(s, EXAMPLE_MAX_SIZE, "s");
     s[EXAMPLE_MAX_SIZE - 1] = '\0';
     
     o = loopFunction(s);
-    m = interpreter(s, argv[1]);
+    m = interpreter(s, prog);
 
-//    printf("s: %p original %p, mine %p \n",s,  o, m);
+    printf("s: %p original %p, mine %p \n",s,  o, m);
     assert(o == m && "Found a bug with  syntehesized program!");
 
     return 0;
