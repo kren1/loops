e4e370025113fd9a5622c77465192fb4d6eed27e
Timotej Kapus <tk1713@ic.ac.uk>
2018-10-01 10:24:33 +0100

    Convience scriptw

--- summarizeLoopInfo.sh
+++ summarizeLoopInfo.sh
@@ -5,5 +5,6 @@ NUM_WRITE_LOOPS=$(grep "write loop" $1 | wc -l)
 NUM_OUTER_LOOPS=$(grep "Non empty" $1 | wc -l)
 let "NUM_INNER_LOOPS=$NUM_LOOPS - $NUM_OUTER_LOOPS"
 POINTER_CALLS=$(grep "pointer call" $1 | wc -l)
+FOUND_LOOPS=$(grep "BINGO!" $1 | wc -l)
 
-echo "$UTILITY_NAME   $NUM_LOOPS   $NUM_INNER_LOOPS   $POINTER_CALLS   $NUM_WRITE_LOOPS "
+echo "$UTILITY_NAME   $NUM_LOOPS   $NUM_INNER_LOOPS   $POINTER_CALLS   $NUM_WRITE_LOOPS  $FOUND_LOOPS"
